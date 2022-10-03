add78e63c35fd67123c8f77ef86bd70240ce9bfa
andrewira <111717557+andrewira@users.noreply.github.com>
2022-10-03 09:41:39 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="login.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  <title>login</title>
+</head>
+<body>
+  
+</body>
+</html>
+<div class="container text-center">
+  <div class="row">
+    <div class="col">
+<div class="login-page">
+  <div class="form">
+    <form class="register-form">
+      <input type="text" placeholder="name"/>
+      <input type="password" placeholder="password"/>
+      <input type="text" placeholder="email address"/>
+      <button>create</button>
+      <p class="message">Already registered? <a href="#">Sign In</a></p>
+    </form>
+    <form class="login-form">
+      <h1>LOGIN</h1>
+      <label for="nama"><b>nama</b></label class=>
+      <input type="text" placeholder="Nama"/> 
+      <input type="password" placeholder="kata sandi"/>
+      <button >masuk</button>
+      <p class="message">tidak memiliki akun? <a href="buatakun.php">buat akun</a></p>
+    </form>
+  </div>
+</div>
+</div>
+  </div>
+</div>
---
+++ toko.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet"href="contoh.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
+    <title>tokopedia</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+  
+   
+</head>
+<body>
+    <nav>
+        <img src="https://ecs7.tokopedia.net/assets-tokopedia-lite/v2/zeus/production/e5b8438b.svg">
+        <nav class="navbar bg-light">
+            <form class="container-fluid">
+              <div class="input-group">
+                
+                <input type="text" class="form-control" placeholder="search..." aria-label="search..." aria-describedby="basic-addon1">
+                <span><button type="button" class="btn btn-outline-success">CARI</button></span>
+                <a class="btn btn-outline-dark" href="login.html" role="button">masuk</a>
+              </div>
+            </form>
+          </nav>
+          </nav>
+            
+        
+    </nav>
+    <div class="carousel-inner">
+        <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+           <div class="carousel-item active">
+             <img src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/0dfb2b2e-cfda-4516-a123-b0117b5417ca.jpg.webp?ect=3g" class="d-block w-100" alt="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/0dfb2b2e-cfda-4516-a123-b0117b5417ca.jpg.webp?ect=3g" class="d-block w-100" alt="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/0dfb2b2e-cfda-4516-a123-b0117b5417ca.jpg.webp?ect=3g" class="d-block w-100" alt="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/0dfb2b2e-cfda-4516-a123-b0117b5417ca.jpg.webp?ect=3g">
+           </div>
+          <div class="carousel-item">
+            <img src="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/96dab3be-f7b0-499b-90a2-de1128a2275e.jpg.webp?ect=3g" class="d-block w-100" alt="https://images.tokopedia.net/img/cache/1208/NsjrJu/2022/9/1/96dab3be-f7b0-499b-90a2-de1128a2275e.jpg.webp?ect=3g">
+          </div>
+          <div class="carousel-item">
+            <img src="https://www.static-src.com/siva/asset//09_2022/dji-avata-carousel-home-desktop-sep.jpg?w=960" class="d-block w-100" alt="https://www.static-src.com/siva/asset//09_2022/dji-avata-carousel-home-desktop-sep.jpg?w=960">
+          </div>
+        </div>
+           <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+      </div>
+    <div class="container text-center">
+        <div class="row row-cols-4">
+          <div class="col">
+            <div class="card" style="width: 13rem;">
+            <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+            <P>BAJU</P>
+            <h5>RP.50.000</h5>
+            <button type="button" class="btn btn-primary">BELI</button>
+            </div>
+            </div>
+         <div class="col">
+          <div class="card" style="width: 13rem;">
+            <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+            <P>BAJU</P>
+            <h5>RP.50.000</h5>
+            <button type="button" class="btn btn-primary">BELI</button>
+            </div>
+            </div>
+            <div class="col">
+              <div class="card" style="width: 13rem;">
+                <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                <P>BAJU</P>
+                <h5>RP.50.000</h5>
+                <button type="butt" class="btn btn-primary">BELI</button>
+                </div>
+                </div>         
+                <div class="col">
+                  <div class="card" style="width: 13rem;">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                    <P>BAJU</P>
+                    <h5>RP.50.000</h5>
+                    <button type="button" class="btn btn-primary">BELI</button>
+                    </div>
+                    </div>
+             <div class="col">
+                <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                <P>BAJU</P>
+                <h5>RP.50.000</h5>
+                <button type="button" class="btn btn-primary">BELI</button>
+                </div>
+                <div class="col">
+                  <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                  <P>BAJU</P>
+                  <h5>RP.50.000</h5>
+                  <button type="button" class="btn btn-primary">BELI</button>
+                  </div>
+                  <div class="col">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                    <P>BAJU</P>
+                    <h5>RP.50.000</h5>
+                    <button type="button" class="btn btn-primary">BELI</button>
+                    </div>
+                 <div class="col">
+                    <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                    <P>BAJU</P>
+                    <h5>RP.50.000</h5>
+                    <button type="button" class="btn btn-primary">BELI</button>
+                    </div>
+                    <div class="col">
+                      <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                      <P>BAJU</P>
+                      <h5>RP.50.000</h5>
+                      <button type="button" class="btn btn-primary">BELI</button>
+                      </div>
+                      <div class="col">
+                        <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                        <P>BAJU</P>
+                        <h5>RP.50.000</h5>
+                        <button type="button" class="btn btn-primary">BELI</button>
+                        </div>
+                     <div class="col">
+                        <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                        <P>BAJU</P>
+                        <h5>RP.50.000</h5>
+                        <button type="button" class="btn btn-primary">BELI</button>
+                        </div>
+                        <div class="col">
+                          <img src="https://images.tokopedia.net/img/cache/200-square/VqbcmM/2022/1/11/48bf6903-f566-4793-8a38-b18476b2aa25.jpg.webp?ect=4g">
+                          <P>BAJU</P>
+                          <h5>RP.50.000</h5>
+                          <button type="button" class="btn btn-primary">BELI</button>
+                          </div>
+                          
+                 </div>
+               </div>  
+              </body>
+              <div>
+
+              </div>
+             </html>
